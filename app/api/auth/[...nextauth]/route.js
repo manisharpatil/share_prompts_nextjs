cc7385a92932d16dfc252ff4c6a79d3ea68de60f
Manisha Patil <manisharpatil6@gmail.com>
2023-07-09 08:35:59 +0530

    fix: added corretion for GOOGLE_ID in route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,10 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-console.log({
-    clientId: process.env.GOOLE_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
-
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOLE_ID,
+            clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
