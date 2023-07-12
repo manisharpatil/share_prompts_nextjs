0925b64303a87961a351d8f1f906c35379b80626
Manisha Patil <manisharpatil6@gmail.com>
2023-07-12 23:37:53 +0530

    feat: added authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
@@ -11,7 +12,11 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        // store the user id from MongoDB to session
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.user.id = sessionUser._id.toString();
 
+        return session;
     },
     async signIn({ profile }) {
         try {
@@ -19,12 +24,21 @@ const handler = NextAuth({
             await connectToDB();
 
             // check if a user already exist
+            const userExists = await User.findOne({ email: profile.email });
 
             // if not, create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture,
+                });
+            }
 
             return true;
         } catch (error) {
-
+            console.log(error);
+            return false;
         }
     }
 });
