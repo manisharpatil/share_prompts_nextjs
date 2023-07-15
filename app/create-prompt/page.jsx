65a42be6439e7340acc83e1a0d1283530ba362f0
Manisha Patil <manisharpatil6@gmail.com>
2023-07-15 19:08:40 +0530

    api: added api endpoints

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -14,7 +14,27 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true);
 
+        try {
+            const response = await fetch("/api/prompt/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag,
+                }),
+            });
+
+            if (response.ok) {
+                router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsSubmitting(false);
+        }
     };
 
     return (
