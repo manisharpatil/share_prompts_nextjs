ed9e41a311cc18c81a37d276da255306f483091e
Manisha Patil <manisharpatil6@gmail.com>
2023-07-16 17:25:13 +0530

    api: added fetch, patch and delete api

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,7 +6,7 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB();
 
-        const prompts = await Prompt.findById(params.id).populate('creator');
+        const prompt = await Prompt.findById(params.id).populate('creator');
         if (!prompt) return new Response("Prompt not found", { status: 404 });
 
         return new Response(JSON.stringify(prompt), {
@@ -27,9 +27,33 @@ export const PATCH = async (request, { params }) => {
         const existingPrompt = await Prompt.findById(params.id);
 
         if (!existingPrompt) return new Response("Prompt not found", { status: 404 });
-    } catch (error) {
 
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPrompt,
+            { status: 200 }));
+    } catch (error) {
+        return new Response("Failed to update prompt", {
+            status: 500
+        });
     }
 };
 
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 });
+
+    } catch (error) {
+        return new Response("Failed to delete prompt", {
+            status: 500
+        });
+    }
+}
